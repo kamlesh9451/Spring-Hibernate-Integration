6ecdcae7c479dace7e9976b18fffd695cc1cad3f
kamlu <kamlu@kamlesh>
2018-10-19 00:07:24 +0530

    Initial commit

---
+++ Spring Boot-Hibernate-Rest/src/main/java/com/kp/Spring/BootHibernateRest/SpringBootHibernateRestApplication.java
@@ -0,0 +1,12 @@
+package com.kp.Spring.BootHibernateRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernateRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHibernateRestApplication.class, args);
+	}
+}
---
+++ Spring Boot-Hibernate-Rest/src/test/java/com/kp/Spring/BootHibernateRest/SpringBootHibernateRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kp.Spring.BootHibernateRest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootHibernateRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
